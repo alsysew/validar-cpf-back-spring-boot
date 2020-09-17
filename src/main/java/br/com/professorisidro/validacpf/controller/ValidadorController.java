406f77f4bd24bc40c6eb7dc01490f1e967b92831
alsysew <andredesanti@gmail.com>
2020-09-17 17:32:08 -0300

    Acerto do map

--- src/main/java/br/com/professorisidro/validacpf/controller/ValidadorController.java
+++ src/main/java/br/com/professorisidro/validacpf/controller/ValidadorController.java
@@ -19,4 +19,10 @@ public class ValidadorController {
 		 return "{\"status\": \"invalido\"}";
 	}
 
+	@GetMapping("/")
+	public String index()) {
+		 return "{\"mensagem\": \"apiitauturma8a\"}";
+	}
+
+
 }
